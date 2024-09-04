9122e60a5600260017854f90ad56b1b79805227b
Yehonatan <yehonatanrefael@gmail.com>
2024-09-04 18:37:05 +0300

    repeat char

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,5 +1,7 @@
 const word = require("./reapet/word");
+const wordDelay = require("./reapet/wordDelay");
+const char = require("./reapet/char");
 module.exports.bot = (msg) => {
   console.log("bot is running");
-  return word(msg);
+  return word(msg) || wordDelay(msg) || char(msg);
 };
